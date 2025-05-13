72e0cd436880e6ba373e6de96f26c08ab5dfea9d
fmfahath <fmfahath.dev@gmail.com>
2025-05-13 23:53:18 +0530

    frontend -- folder structure created, files added++

---
+++ src/components/TitleCard/TitleCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TitleCard.css'
+
+const TitleCard = () => {
+    return (
+        <div className='titleCard'>TitleCard</div>
+    )
+}
+
+export default TitleCard 
