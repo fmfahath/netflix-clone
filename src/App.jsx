5c43b05817a902d7a2640ec14a4d416fb3e568e5
fmfahath <fmfahath.dev@gmail.com>
2025-05-15 16:42:44 +0530

    frontend -- Player component created, css added, route added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './pages/home/Home'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/login/Login'
+import Player from './pages/player/Player'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player />} />
       </Routes>
     </div>
   )
--- src/pages/player/Player.jsx
+++ src/pages/player/Player.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
     return (
-        <div className='player'>Player</div>
+        <div className='player'>
+            <img src={back_arrow_icon} alt="" />
+            <iframe
+                width='90%'
+                height='90%'
+                src="https://www.youtube.com/embed/2lAe1cqCOXo"
+                title="Trailer"
+                allowFullScreen
+                frameborder="0">
+            </iframe>
+            <div className="player-info">
+                <p>Published Date</p>
+                <p>Name</p>
+                <p>Type</p>
+            </div>
+        </div>
     )
 }
 
