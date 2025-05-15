8a1720e99b87999a09c29ff9153901bd9c20e500
fmfahath <fmfahath.dev@gmail.com>
2025-05-15 15:20:49 +0530

    frontend -- Login sign In / sign Up state change state added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -25,7 +25,9 @@ const Login = () => {
                     </div>
                 </form>
                 <div className='form-switch'>
-                    {signState === "Sign In" ? <p>New to Netflix? <span>Sign Up Now</span></p> : <p>Already have Account? <span>Sign In Now</span></p>}
+                    {signState === "Sign In" ?
+                        <p>New to Netflix? <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span></p> :
+                        <p>Already have Account? <span onClick={() => setSignState("Sign In")}>Sign In Now</span></p>}
 
 
                 </div>
