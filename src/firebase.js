ce9da91453f33e21efc10e96165f0d7b3450b95a
fmfahath <fmfahath.dev@gmail.com>
2025-05-15 21:34:29 +0530

    backend -- signup, login, logout fuc created in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { addDoc, getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,3 +15,43 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+
+// signup function------------------------------------------------
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "users"), {
+            uid: user.uid,
+            name: name,
+            authProvider: "local",
+            email: email
+        });
+    }
+    catch (error) {
+        console.log(error);
+        alert(error.message);
+
+    }
+}
+
+
+// login function------------------------------------------------
+const login = async (email, password) => {
+    try {
+        const res = await signInWithEmailAndPassword(auth, email, password);
+    }
+    catch (error) {
+        console.log(error);
+        alert(error.message);
+    }
+}
+
+// logout function------------------------------------------------
+const logout = () => {
+    signOut(auth);
+}
+
+
+export { auth, db, login, logout, signup };
