65d0b79459d766c15a42142847e760dadd28c60b
fmfahath <fmfahath.dev@gmail.com>
2025-05-15 22:23:51 +0530

    frontend -- onAuthStateChange func added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/home/Home'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/login/Login'
 import Player from './pages/player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("logged in");
+        navigate('/')
+      }
+      else {
+        console.log("logged Out");
+        navigate('/login')
+      }
+    })
+  }, [])
+
   return (
     <div>
       <Routes>
